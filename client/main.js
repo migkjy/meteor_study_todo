1559c8c21cde1b42aa421c6a86d4f68316f52798
Junyoung Kim <migkjy@naver.com>
2017-10-30 18:22:06 +0900

    around at 18 minuites

--- client/main.js
+++ client/main.js
@@ -1,13 +1,10 @@
 import { Template } from 'meteor/templating';
+import { Notes } from '../lib/collections.js';
+
 import './main.html';
 
-Template.note.helpers({
-  notes: [
-    { text: 'My note 1' },
-    { text: 'My note 2' },
-    { text: 'My note 3' },
-  ],
-  myFunction() {
-
+Template.body.helpers({
+  notes() {
+    return Notes.find({});
   },
 });
