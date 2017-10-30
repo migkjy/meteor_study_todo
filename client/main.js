2dbf9d87a830882d6aa23182fa2e90f4dfe70edb
Junyoung Kim <migkjy@naver.com>
2017-10-30 19:21:11 +0900

    intelliSense for css install

--- client/main.js
+++ client/main.js
@@ -1,10 +1,44 @@
 import { Template } from 'meteor/templating';
 import { Notes } from '../lib/collections.js';
+import { Accounts } from 'meteor/accounts-base';
 
 import './main.html';
 
 Template.body.helpers({
   notes() {
     return Notes.find({});
+  },
+});
+
+Template.add.events({
+  'submit .add-form': function () {
+    event.preventDefault();
+    // console.log(123);
+
+    // Get input value
+    const target = event.target;
+    const text = target.text.value;
+    // console.log(text);
+
+    // Insert note into collection
+    Notes.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    // Clear form
+    target.text.value = '';
+
+    // Close modal
+    $('#addModal').modal('close');
+
+    return false;
+  },
+});
+
+Template.note.events({
+  'click .delete-note': function () {
+    Notes.remove(this._id);
+    return false;
   },
 });
